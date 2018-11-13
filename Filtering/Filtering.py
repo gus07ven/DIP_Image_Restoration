1f7dc3a1a7ba69388a1233d8f0430b5586522ad8
zkpzkp1234 <zkpzkp1234>
2018-11-12 23:52:09 -0600

    mean filter -- arithmetic mean, geometric mean, padding

--- Filtering/Filtering.py
+++ Filtering/Filtering.py
@@ -8,74 +8,63 @@ from decimal import Decimal
 class Filtering:
     image = None
 
+    def __init__(self, image):
+        """initializes the variables frequency filtering on an input image
+        takes as input:
+        image: the input image
+        filter_name: the name of the mask to use
+        cutoff: the cutoff frequency of the filter
+        order: the order of the filter (only for butterworth
+        returns"""
+        self.image = image
+
+    def padding_img(self, img, windowSize = 3):
+        height, width = img.shape[:2]
+        margin = int(windowSize / 2)
+        paddingImage = np.zeros((height + margin * 2, width + margin * 2), np.uint8)
+        for j in range(margin):
+                paddingImage[j,margin:margin+width] = img[0,0:width]
+                paddingImage[j+height+margin,margin:margin+width] = img[height-1,0:width]
+
+        paddingImage[margin:margin+height,margin:margin+width] = img[:]
+
+        for j in range(margin):
+            paddingImage[0:margin*2+height,j]=paddingImage[0:margin*2+height,margin]
+            paddingImage[0:margin * 2 + height, j+width+margin] = paddingImage[0:margin * 2 + height, width+margin-1]
+
+        return paddingImage
+
     # apply 3*3 mean filter
     def arithmetic_mean_filter(self, img, windowSize = 3):
         height, width = img.shape[:2]
-        newimg = np.zeros(img.shape[:2], np.uint8)
-        # mask = np.zeros((3, 3), np.uint8)
+        margin = int(windowSize/2)
+
+        paddingImg = self.padding_img(img, windowSize)
+        newImg = np.zeros(img.shape[:2],np.uint8)
+
         for j in range(height):
             for i in range(width):
-                if 0 < i < width - 1 and 0 < j < height - 1:
-                    mask = np.zeros((3, 3), np.uint8)
-                    mask[0:3, 0:3] = img[j - 1:j + 2, i - 1:i + 2]
+                mask = np.zeros((windowSize, windowSize), np.uint8)
+                mask[0:windowSize, 0:windowSize] = paddingImg[j:j + windowSize, i:i + windowSize]
+                average = np.sum(mask) / (windowSize**2)
+                newImg[j][i] = int(average+0.5)
+        return newImg
 
-                elif i == 0 and 0 < j < height - 1:
-                    mask = np.zeros((3, 3), np.uint8)
-                    mask[0:3, 1:3] = img[j - 1:j + 2, i:i + 2]
-                    mask[0:3, 0] = img[j - 1:j + 2, i]
-                elif i == width - 1 and 0 < j < height - 1:
-                    mask = np.zeros((3, 3), np.uint8)
-                    mask[0:3, 0:2] = img[j - 1:j + 2, i - 1:i + 2]
-                    mask[0:3, 2] = img[j - 1:j + 2, i]
-
-                elif 0 < i < width - 1 and j == 0:  # ok
-                    mask = np.zeros((3, 3), np.uint8)
-                    mask[1:3, :] = img[j:j + 2, i - 1:i + 2]
-                    mask[0, :] = img[j, i - 1:i + 2]
-
-                elif 0 < i < width - 1 and j == height - 1:
-                    mask = np.zeros((3, 3), np.uint8)
-                    mask[0:2, 0:3] = img[j - 1:j + 2, i - 1:i + 2]
-                    mask[2, 0:3] = img[j, i - 1:i + 2]
-                else:
-                    mask = np.full((3, 3), img[j][i])
-                average = np.sum(mask) / 9
-                newimg[j][i] = int(average)
-        return newimg
-
-    def geometric_mean_filter(self, img):
+    def geometric_mean_filter(self, img, windowSize = 3):
         height, width = img.shape[:2]
-        newimg = np.zeros(img.shape[:2], np.uint8)
+        margin = int(windowSize / 2)
+
+        paddingImg = self.padding_img(img, windowSize)
+        newImg = np.zeros(img.shape[:2], np.uint8)
+
         # mask = np.zeros((3, 3), np.uint8)
         for j in range(height):
             for i in range(width):
-                if 0 < i < width - 1 and 0 < j < height - 1:
-                    mask = np.zeros((3, 3), np.uint8)
-                    mask[0:3, 0:3] = img[j - 1:j + 2, i - 1:i + 2]
-
-                elif i == 0 and 0 < j < height - 1:
-                    mask = np.zeros((3, 3), np.uint8)
-                    mask[0:3, 1:3] = img[j - 1:j + 2, i:i + 2]
-                    mask[0:3, 0] = img[j - 1:j + 2, i]
-                elif i == width - 1 and 0 < j < height - 1:
-                    mask = np.zeros((3, 3), np.uint8)
-                    mask[0:3, 0:2] = img[j - 1:j + 2, i - 1:i + 2]
-                    mask[0:3, 2] = img[j - 1:j + 2, i]
-
-                elif 0 < i < width - 1 and j == 0:  # ok
-                    mask = np.zeros((3, 3), np.uint8)
-                    mask[1:3, :] = img[j:j + 2, i - 1:i + 2]
-                    mask[0, :] = img[j, i - 1:i + 2]
-
-                elif 0 < i < width - 1 and j == height - 1:
-                    mask = np.zeros((3, 3), np.uint8)
-                    mask[0:2, 0:3] = img[j - 1:j + 2, i - 1:i + 2]
-                    mask[2, 0:3] = img[j, i - 1:i + 2]
-                else:
-                    mask = np.full((3, 3), img[j][i])
-                multiple = Decimal(1)
-                for l in range(3):
-                    for m in range(3):
-                        multiple *= mask[l][m]
-                newimg[j][i] = int(math.pow(multiple, 1 / 9))
-        return newimg
+                mask = np.zeros((windowSize, windowSize), np.uint8)
+                mask[0:windowSize, 0:windowSize] = paddingImg[j:j + windowSize, i:i + windowSize]
+                multipleValue = Decimal(1)
+                for l in range(windowSize):
+                    for m in range(windowSize):
+                        multipleValue *= mask[l][m]
+                newImg[j][i] = int(math.pow(multipleValue, windowSize**(-2))+0.5)
+        return newImg
