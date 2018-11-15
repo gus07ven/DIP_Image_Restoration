eba121acec85d06299c606736ec512bd79140173
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2018-11-15 01:13:07 -0600

    Adaptive median filter complete

--- Filtering/Filtering.py
+++ Filtering/Filtering.py
@@ -118,70 +118,57 @@ class Filtering:
                 newImg[j][i] = int(valueNom/valueDen + 0.5)
         return newImg
 
-    def median_filter(self, img, windowSize=3):
+    def median_filter(self, img, window_size=3):
         height, width = img.shape[:2]
-        margin = int(windowSize / 2)
-
-        paddingImg = self.padding_img(img, windowSize)
-        newImg = np.zeros(img.shape[:2], np.uint8)
+        padding_img = self.padding_img(img, window_size)
+        new_img = np.zeros(img.shape[:2], np.uint8)
 
         for j in range(height):
             for i in range(width):
-                mask = np.zeros((windowSize, windowSize), np.uint8)
-                mask[0:windowSize, 0:windowSize] = paddingImg[j:j + windowSize, i:i + windowSize]
-                median = np.ma.median(np.squeeze(np.asarray(mask)))    # we can implement getMedian if necessary
-                newImg[j][i] = median
-        return newImg
+                mask = np.zeros((window_size, window_size), np.uint8)
+                mask[0:window_size, 0:window_size] = padding_img[j:j + window_size, i:i + window_size]
+                new_img[j][i] = np.ma.median(np.squeeze(np.asarray(mask)))    # we can implement getMedian if necessary
+        return new_img
 
-    def max_filter(self, img, windowSize=3):
+    def max_filter(self, img, window_size=3):
         height, width = img.shape[:2]
-        margin = int(windowSize / 2)
-
-        paddingImg = self.padding_img(img, windowSize)
-        newImg = np.zeros(img.shape[:2], np.uint8)
+        padding_img = self.padding_img(img, window_size)
+        new_img = np.zeros(img.shape[:2], np.uint8)
 
         for j in range(height):
             for i in range(width):
-                mask = np.zeros((windowSize, windowSize), np.uint8)
-                mask[0:windowSize, 0:windowSize] = paddingImg[j:j + windowSize, i:i + windowSize]
-                max = mask.max()  # we can implement getMax if necessary
-                newImg[j][i] = max
-        return newImg
+                mask = np.zeros((window_size, window_size), np.uint8)
+                mask[0:window_size, 0:window_size] = padding_img[j:j + window_size, i:i + window_size]
+                new_img[j][i] = mask.max()  # we can implement getMax if necessary
+        return new_img
 
-    def min_filter(self, img, windowSize=3):
+    def min_filter(self, img, window_size=3):
         height, width = img.shape[:2]
-        margin = int(windowSize / 2)
-
-        paddingImg = self.padding_img(img, windowSize)
-        newImg = np.zeros(img.shape[:2], np.uint8)
+        padding_img = self.padding_img(img, window_size)
+        new_img = np.zeros(img.shape[:2], np.uint8)
 
         for j in range(height):
             for i in range(width):
-                mask = np.zeros((windowSize, windowSize), np.uint8)
-                mask[0:windowSize, 0:windowSize] = paddingImg[j:j + windowSize, i:i + windowSize]
-                min = mask.min()  # we can implement getMin if necessary
-                newImg[j][i] = min
-        return newImg
+                mask = np.zeros((window_size, window_size), np.uint8)
+                mask[0:window_size, 0:window_size] = padding_img[j:j + window_size, i:i + window_size]
+                new_img[j][i] = mask.min()  # we can implement getMin if necessary
+        return new_img
 
-    def midpoint_filter(self, img, windowSize=3):
+    def midpoint_filter(self, img, window_size=3):
         height, width = img.shape[:2]
-        margin = int(windowSize / 2)
-
-        paddingImg = self.padding_img(img, windowSize)
-        newImg = np.zeros(img.shape[:2], np.uint8)
+        padding_img = self.padding_img(img, window_size)
+        new_img = np.zeros(img.shape[:2], np.uint8)
 
         for j in range(height):
             for i in range(width):
-                mask = np.zeros((windowSize, windowSize), np.uint8)
-                mask[0:windowSize, 0:windowSize] = paddingImg[j:j + windowSize, i:i + windowSize]
+                mask = np.zeros((window_size, window_size), np.uint8)
+                mask[0:window_size, 0:window_size] = padding_img[j:j + window_size, i:i + window_size]
                 midpoint = (mask.max() + mask.min()) / 2  # Getting rid of the decimal part. Should we add + .5?
-                newImg[j][i] = midpoint
-        return newImg
+                new_img[j][i] = midpoint
+        return new_img
 
     def alpha_trimmed_filter(self, img, window_size=3):
         height, width = img.shape[:2]
-        margin = int(window_size / 2)
-
         padding_img = self.padding_img(img, window_size)
         new_img = np.zeros(img.shape[:2], np.uint8)
         d = 2  # Should we make this a user determined parameter? If so, we have to be careful
@@ -220,4 +207,46 @@ class Filtering:
                 else:
                     newImg[j][i] = img[j][i] - theta_square_n/localVariance*(img[j][i] - localMean)
         return newImg
+
+    def adaptive_median_filter(self, img, window_size=3):
+        height, width = img.shape[:2]
+        padding_image = self.padding_img(img, window_size)
+        new_img = np.zeros(img.shape[:2], np.uint8)
+
+        for j in range(height):
+            for i in range(width):
+                mask = np.zeros((window_size, window_size), np.uint8)
+                mask[0: window_size, 0: window_size] = padding_image[j: j + window_size, i: i + window_size]
+                z_min = mask.min()
+                z_max = mask.max()
+                z_med = int(np.ma.median(np.squeeze(np.asarray(mask))))
+                z_xy = np.asarray(img)[j][i]
+                s_max = 7
+
+                test = self.level_a(j, i, z_med, z_min, z_max, z_xy, window_size, s_max)
+                new_img[j][i] = test
+        return new_img
+
+    def level_a(self, j, i, z_med, z_min, z_max, z_xy, window_size, s_max):
+        a_1 = z_med - z_min
+        a_2 = z_med - z_max
+        if a_2 < 0 < a_1:
+            return self.level_b(z_med, z_min, z_max, z_xy)
+        else:
+            window_size += 2
+            if window_size <= s_max:
+                return self.level_a(j, i, z_med, z_min, z_max, z_xy, window_size, s_max)
+            else:
+                return z_xy
+
+    def level_b(self, z_med, z_min, z_max, z_xy):
+        b_1 = z_xy - z_min
+        b_2 = z_xy - z_max
+        if b_2 < 0 < b_1:
+            return z_xy
+        else:
+            return z_med
+
+
+
 
