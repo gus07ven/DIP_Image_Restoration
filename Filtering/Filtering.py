a5d3cc60d7fd53d5dbeaf7ba7e71a65fe17d0f57
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2018-11-14 18:01:46 -0600

    Complete OSF alpha_trimmed_filter

--- Filtering/Filtering.py
+++ Filtering/Filtering.py
@@ -178,6 +178,24 @@ class Filtering:
                 newImg[j][i] = midpoint
         return newImg
 
+    def alpha_trimmed_filter(self, img, window_size=3):
+        height, width = img.shape[:2]
+        margin = int(window_size / 2)
+
+        padding_img = self.padding_img(img, window_size)
+        new_img = np.zeros(img.shape[:2], np.uint8)
+        d = 2  # Should we make this a user determined parameter? If so, we have to be careful
+
+        for j in range(height):
+            for i in range(width):
+                mask = np.zeros((window_size, window_size), np.uint8)
+                mask[0:window_size, 0:window_size] = padding_img[j:j + window_size, i:i + window_size]
+                mask_to_ordered_array = np.sort(np.asarray(mask).flatten())
+                new_mask = mask_to_ordered_array[int(d/2): mask_to_ordered_array.size - (int(d/2))]
+                alpha_trimmed_mean = int(np.ma.mean(np.squeeze(new_mask)))  # Should we cast to int when np takes care of it?
+                new_img[j][i] = alpha_trimmed_mean
+        return new_img
+
     def adaptive_local_noise_reduction_filter(self, img, theta_square_n, windowSize = 3):
         height, width = img.shape[:2]
         margin = int(windowSize / 2)
@@ -203,62 +221,3 @@ class Filtering:
                     newImg[j][i] = img[j][i] - theta_square_n/localVariance*(img[j][i] - localMean)
         return newImg
 
-    def median_filter(self, img, windowSize=3):
-        height, width = img.shape[:2]
-        margin = int(windowSize / 2)
-
-        paddingImg = self.padding_img(img, windowSize)
-        newImg = np.zeros(img.shape[:2], np.uint8)
-
-        for j in range(height):
-            for i in range(width):
-                mask = np.zeros((windowSize, windowSize), np.uint8)
-                mask[0:windowSize, 0:windowSize] = paddingImg[j:j + windowSize, i:i + windowSize]
-                median = np.ma.median(np.squeeze(np.asarray(mask)))    # we can implement getMedian if necessary
-                newImg[j][i] = median
-        return newImg
-
-    def max_filter(self, img, windowSize=3):
-        height, width = img.shape[:2]
-        margin = int(windowSize / 2)
-
-        paddingImg = self.padding_img(img, windowSize)
-        newImg = np.zeros(img.shape[:2], np.uint8)
-
-        for j in range(height):
-            for i in range(width):
-                mask = np.zeros((windowSize, windowSize), np.uint8)
-                mask[0:windowSize, 0:windowSize] = paddingImg[j:j + windowSize, i:i + windowSize]
-                max = mask.max()  # we can implement getMax if necessary
-                newImg[j][i] = max
-        return newImg
-
-    def min_filter(self, img, windowSize=3):
-        height, width = img.shape[:2]
-        margin = int(windowSize / 2)
-
-        paddingImg = self.padding_img(img, windowSize)
-        newImg = np.zeros(img.shape[:2], np.uint8)
-
-        for j in range(height):
-            for i in range(width):
-                mask = np.zeros((windowSize, windowSize), np.uint8)
-                mask[0:windowSize, 0:windowSize] = paddingImg[j:j + windowSize, i:i + windowSize]
-                min = mask.min()  # we can implement getMin if necessary
-                newImg[j][i] = min
-        return newImg
-
-    def midpoint_filter(self, img, windowSize=3):
-        height, width = img.shape[:2]
-        margin = int(windowSize / 2)
-
-        paddingImg = self.padding_img(img, windowSize)
-        newImg = np.zeros(img.shape[:2], np.uint8)
-
-        for j in range(height):
-            for i in range(width):
-                mask = np.zeros((windowSize, windowSize), np.uint8)
-                mask[0:windowSize, 0:windowSize] = paddingImg[j:j + windowSize, i:i + windowSize]
-                midpoint = (mask.max() + mask.min()) / 2  # Getting rid of the decimal part. Should we add + .5?
-                newImg[j][i] = midpoint
-        return newImg
